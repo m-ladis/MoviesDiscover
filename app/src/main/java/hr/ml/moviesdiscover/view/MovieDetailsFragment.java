4301cb2af71f5b156e3d6879c6b7882785793de7
ml <marinoladis@hotmail.com>
2021-04-02 13:02:56 +0200

    Implemented nav graph with two empty fragments

---
+++ app/src/main/java/hr/ml/moviesdiscover/view/MovieDetailsFragment.java
@@ -0,0 +1,30 @@
+package hr.ml.moviesdiscover.view;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import hr.ml.moviesdiscover.R;
+
+public class MovieDetailsFragment extends Fragment {
+
+    public MovieDetailsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_movie_details, container, false);
+    }
+}
